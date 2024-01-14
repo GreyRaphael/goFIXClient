513e1f19dce52d1b6d871a5211d8b01dc43ca2bb
BeGeWei <grey@pku.edu.cn>
2024-01-14 22:01:03 +0800

    simple program

--- main.go
+++ main.go
@@ -4,27 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	client := TradeClient{ConfigFilename: "clients/test/dev.cfg"}
 	client.Start()
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("cmds: exit, o:order, s:strategy, b:basket")
+	fmt.Println("cmds: exit, o:order, s:strategy, b:basket, l:orderlist")
 	for scanner.Scan() {
 		switch scanner.Text() {
 		case "exit":
 			client.Stop()
 			os.Exit(0)
 		case "o":
-			client.SendOrder("1", "600000.SS", 100, 6.0)
+			orderid := client.SendOrder("1", "600000.SS", 100, 6.0)
+			client.CancelOrder(orderid)
 		case "s":
 			for i := 0; i < 500; i++ {
 				client.SendOrder("1", "600000.SS", 100, 6.0)
 			}
 		case "b":
 			client.SendBasket("1", "input/zz500.csv", 1)
+			time.Sleep(time.Second)
+			client.CancelAll()
+		case "l":
+			client.SendOrderList("order_list1")
 		}
-		// time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 3)
+		fmt.Println("cmds: exit, o:order, s:strategy, b:basket, l:orderlist")
 	}
 }
