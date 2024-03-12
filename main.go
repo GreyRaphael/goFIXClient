a84e7f1b2a7c6a7db00590c182eb37110544b857
KT_gewei <grey@pku.edu.cn>
2024-03-12 15:42:07 +0800

    add algo

--- main.go
+++ main.go
@@ -11,18 +11,21 @@ import (
 func main() {
 	input := flag.String("i", "input/single.csv", "input file(*.csv)")
 	batchSize := flag.Int("n", 1, "the total number of batches")
+	cfg := flag.String("c", "clients/test/swap.cfg", "client config file")
 	flag.Parse()
 
-	client := TradeClient{ConfigFilename: "clients/test/swap.cfg"}
+	client := TradeClient{ConfigFilename: *cfg}
 	client.Start()
 
-	fmt.Println("cmds: e:exit, b:buy, s:sell, c:cancel_all, x:cancel_byid")
+	fmt.Println("cmds: e:exit, a:algo, b:buy, s:sell, c:cancel_all, x:cancel_byid")
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		switch strings.ToLower(scanner.Text()) {
 		case "e": // exit
 			client.Stop()
 			os.Exit(0)
+		case "a": // algorithm
+			client.SendAlgo("1", *input, *batchSize)
 		case "b": // buy
 			client.SendBasket("1", *input, *batchSize)
 		case "s": // sell
