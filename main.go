011a23c3ab5e819ccfa6e7501febff9515063227
BeGeWei <grey@pku.edu.cn>
2024-03-26 00:46:51 +0800

    optimize cli commands

--- main.go
+++ main.go
@@ -9,36 +9,57 @@ import (
 )
 
 func SingleClient() {
-	input := flag.String("i", "input/single.csv", "input file(*.csv)")
-	batchSize := flag.Int("n", 1, "the total number of batches")
 	cfg := flag.String("c", "clients/test/local.cfg", "client config file")
-	orderType := flag.String("t", "1", "order type, 1: DMA; 2: DMA2; 3: CARE; 4: DSA")
 	flag.Parse()
 
 	client := TradeClient{ConfigFilename: *cfg}
 	client.Start()
 
-	fmt.Println("cmds: e:exit, a:algo, b:buy, s:sell, c:cancel_all, x:cancel_byid")
+	// subcommands: order
+	orderCmd := flag.NewFlagSet("order", flag.ContinueOnError)
+	direction := orderCmd.String("d", "1", "direction: 1,buy; 2,sell")
+	handlInst := orderCmd.String("t", "1", "HandlInst: 1,DMA; 2,DMA2; 3,CARE; 4:DSA")
+	input := orderCmd.String("i", "input/single.csv", "stock info file(*.csv)")
+	batNum := orderCmd.Int("n", 1, "batch number")
+	algo := orderCmd.String("a", "1", "algo: 1,direct; 2,latency")
+
+	// subcommands: cancel
+	cancelCmd := flag.NewFlagSet("cancel", flag.ContinueOnError)
+	origOrdId := cancelCmd.String("o", "-1", "cancel order: -1, cancel all; other, cancel specified")
+
+	fmt.Println("Enter subcmds: order(o), cancel(c), exit(e)")
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		switch strings.ToLower(scanner.Text()) {
-		case "e": // exit
+		line := strings.ToLower(scanner.Text())
+		args := strings.Fields(line)
+		fmt.Println(strings.Repeat("=", 60))
+		switch args[0] {
+		case "order", "o":
+			err := orderCmd.Parse(args[1:])
+			if err == nil {
+				if *algo == "direct" {
+					client.SendBasket(*direction, *input, *batNum, *handlInst)
+				} else if *algo == "latency" {
+					client.SendAlgo(*direction, *input, *batNum, *handlInst)
+				}
+			}
+		case "cancel", "c":
+			err := cancelCmd.Parse(args[1:])
+			if err == nil {
+				if *origOrdId == "-1" {
+					client.CancelAll()
+				} else {
+					client.CancelOrder(*origOrdId)
+				}
+			}
+		case "exit", "e":
 			client.Stop()
 			os.Exit(0)
-		case "a": // algorithm
-			client.SendAlgo("1", *input, *batchSize, *orderType)
-		case "b": // buy
-			client.SendBasket("1", *input, *batchSize, *orderType)
-		case "s": // sell
-			client.SendBasket("2", *input, *batchSize, *orderType)
-		case "c":
-			client.CancelAll()
-		case "x":
-			fmt.Println("Enter origid:")
-			var origid string
-			fmt.Scan(&origid)
-			client.CancelOrder(origid)
+		default:
+			fmt.Println("unknown command")
 		}
+		fmt.Println(strings.Repeat("=", 60))
+		fmt.Println("Enter subcmds: order(o), cancel(c), exit(e)")
 	}
 }
 
